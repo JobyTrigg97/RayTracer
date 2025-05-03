97189f0647ea430677a4d157c0db737c0430395d
JobyTrigg97 <jobytrigg97@gmail.com>
2025-05-03 17:10:47 +1000

    Created scene class and added ray colour function to ray

--- src/Ray.kt
+++ src/Ray.kt
@@ -1,1 +1,13 @@
-data class Ray(val origin: Vector3D, val direction: Vector3D)
+class Ray(val origin: Vector3D, val direction: Vector3D){
+
+    fun rayColor(world: Scene): Vector3D {
+        val rec = world.hit(this)
+        return if (rec != null) {
+            Vector3D(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1) * 0.5
+        } else {
+            val unitDir = direction.unit()
+            val t = 0.5 * (unitDir.y + 1.0)
+            Vector3D(1.0, 1.0, 1.0) * (1.0 - t) + Vector3D(0.5, 0.7, 1.0) * t
+        }
+    }
+}
---
+++ src/Scene.kt
@@ -0,0 +1,14 @@
+class Scene(private val objects: List<Hittable>) {
+    fun hit(ray: Ray, tMin: Double = 0.001, tMax: Double = Double.MAX_VALUE) : HitRecord?{
+        var closestSoFar = tMax
+        var hitRecord: HitRecord? = null
+
+        for (obj in objects){
+            val rec = obj.hit(ray, tMin, closestSoFar)
+            if (rec != null)
+                closestSoFar = rec.t
+                hitRecord = rec
+        }
+        return hitRecord
+    }
+}
