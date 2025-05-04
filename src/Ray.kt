c8b91daf1abc799397c7255643b41a732d9c9cc0
JobyTrigg97 <89447814+JobyTrigg97@users.noreply.github.com>
2025-05-04 18:32:11 +1000

    updated rays to recursively scatter based on a depth limit

--- src/Ray.kt
+++ src/Ray.kt
@@ -1,13 +1,20 @@
+import kotlin.let
+
 class Ray(val origin: Vector3D, val direction: Vector3D){
 
-    fun rayColor(world: Scene): Vector3D {
+    fun rayColor(world: Scene, depth: Int): Vector3D {
+        if (depth <= 0) return Vector3D(0.0, 0.0, 0.0)
         val hitRecord = world.hit(this)
-        return if (hitRecord != null) {
-            Vector3D(hitRecord.surfaceNormal.x + 1, hitRecord.surfaceNormal.y + 1, hitRecord.surfaceNormal.z + 1) * 0.5
-        } else {
-            val unitDir = direction.unit()
-            val t = 0.5 * (unitDir.y + 1.0)
-            Vector3D(1.0, 1.0, 1.0) * (1.0 - t) + Vector3D(0.5, 0.7, 1.0) * t
+        hitRecord?.let {
+            val scatterResult = it.material.scatter(this, it)
+            scatterResult?.let { (scattered, attenuation) ->
+                return (attenuation * scattered.rayColor(world, depth - 1))
+            }
+            return Vector3D(0.0, 0.0, 0.0)
         }
+        val unitDir = direction.unit()
+        val t = 0.5 * (unitDir.y + 1.0)
+        return Vector3D(1.0, 1.0, 1.0) * (1.0 - t) + Vector3D(0.5, 0.7, 1.0) * t
+
     }
 }
